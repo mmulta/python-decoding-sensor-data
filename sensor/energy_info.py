f1d869d42988180b8c732cd03e8562d51bc3a313
mmulta <omer.multazeem@icims.com>
2022-05-24 02:46:45 +0530

    Module 5

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -7,13 +7,13 @@ class EnergyData(HouseInfo):
 
     def _get_energy(self,rec):
         energy=int(rec,base=16)
-        energy=energy & ENERGY_BITS
+        energy=energy & self.ENERGY_BITS
         energy=energy>>4
         return energy
     def _convert_data(self,data):
         recs=[]
         for rec in data:
-            recs.append(float(rec))
+            recs.append(self._get_energy(rec))
         return recs
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("energy_usage", rec_area)
@@ -23,4 +23,5 @@ class EnergyData(HouseInfo):
         recs = super().get_data_by_date("energy_usage", rec_date)
         return self._convert_data(recs)
     def calculate_energy_usage(self,data):
-        total_energy=sum([field*ENERGY_PER_BULB for field in)
+        total_energy=sum([field*self.ENERGY_PER_BULB for field in data])
+        return total_energy
